d3ce1191119d982d8a27670727052aaff31ea429
zsiros91 <zsiros91@hotmail.com>
2018-11-28 23:34:43 +0000

    Updated test.c

--- test.c
+++ test.c
@@ -18,30 +18,43 @@ int main(){
 	signal(SIGUSR1, handler);
 
 	int status;
+	int child1_pid;
+	int child2_pid;
+	int child3_pid;
+
+	pid_t child3;
 	pid_t child2;
-	pid_t child1=fork(); //forks make a copy of variables
+	pid_t child1=fork();
 	if (child1<0){perror("The fork calling was not succesful\n"); exit(1);} 
 	if (child1>0){
 		child2=fork();
 		if(child2>0){
-			pause();
+			child3=fork();
+			if(child3>0){ //parent process
+				//pause();
+				
+				printf("parent\n");
+				printf("pid: %d\n\n",getpid());
+				waitpid(child1,&status,0);
+				waitpid(child2,&status,0);
+				waitpid(child3,&status,0);
+				//wait(&status);
+			}else{ //child3 process
+				
+				printf("child3\n");
+				printf("pid: %d\nppid: %d\n\n",getpid(),getppid());
+				//kill(getppid(),SIGUSR1);
 
-			printf("parent\n");
-			printf("pid: %d\nppid: %d\n\n",getpid(),getppid());
-			waitpid(child2,&status,0);
-			waitpid(child1,&status,0);
-
+			}
 		}else{ //child2 process
 			printf("child2\n");
 			printf("pid: %d\nppid: %d\n\n",getpid(),getppid());
-			kill(getppid(),SIGUSR1);
+			//kill(getppid(),SIGUSR1);
 		}
-	}
-	else //child1 process
-	{
+	}else{ //child1 process
 		printf("child1\n");
 		printf("pid: %d\nppid: %d\n\n",getpid(),getppid());
-		kill(getppid(),SIGUSR1);
+		//kill(getppid(),SIGUSR1);
 	}
 	return 0;
 }
