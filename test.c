fbb2ef650b5de9e6d132b143b4c37684806d6706
zsiros91 <zsiros91@hotmail.com>
2018-11-29 22:42:44 +0000

    Updated test.c

--- test.c
+++ test.c
@@ -10,51 +10,58 @@
 int c=0;
 
 void handler(int signumber){
-	c++;
-	printf("Signal with number %i has arrived %dX\n",signumber,c);
+   c++;
+   printf("Signal with number %i has arrived %dX\n",signumber,c);
 }
 
 int main(){
-	signal(SIGUSR1, handler);
+signal(SIGUSR1, handler);
 
-	int status;
-	int child1_pid;
-	int child2_pid;
-	int child3_pid;
+pid_t child3;
+pid_t child2;
+pid_t child1=fork();
+if (child1<0){perror("The fork calling was not succesful\n"); exit(1);}
+if (child1>0){
+    child2=fork();
+    if(child2>0){
+            child3=fork();
+            if(child3>0){ //parent process
+                int status;
+                while(c<3){
+                	
+                }
+				sleep(5);
+                printf("Signal recieved 1...\n");
 
-	pid_t child3;
-	pid_t child2;
-	pid_t child1=fork();
-	if (child1<0){perror("The fork calling was not succesful\n"); exit(1);} 
-	if (child1>0){
-		child2=fork();
-		if(child2>0){
-			child3=fork();
-			if(child3>0){ //parent process
-				//pause();
-				
-				printf("parent\n");
-				printf("pid: %d\n\n",getpid());
-				waitpid(child1,&status,0);
-				waitpid(child2,&status,0);
-				waitpid(child3,&status,0);
-				//wait(&status);
-			}else{ //child3 process
-				
-				printf("child3\n");
-				printf("pid: %d\nppid: %d\n\n",getpid(),getppid());
-				//kill(getppid(),SIGUSR1);
+                    //wait(&status);
 
-			}
+                printf("parent\n");
+                printf("pid: %d\n\n",getpid());
+
+                waitpid(child3,&status,0);
+                waitpid(child2,&status,0);
+                waitpid(child1,&status,0);
+                //wait(&status);
+                printf("End of parent!\n");
+            }else{ //child3 process
+                printf("child3\n");
+                printf("pid: %d\nppid: %d\n\n",getpid(),getppid());
+                printf("Send signal...\n");
+                kill(getppid(),SIGUSR1);
+    		}
 		}else{ //child2 process
 			printf("child2\n");
 			printf("pid: %d\nppid: %d\n\n",getpid(),getppid());
-			//kill(getppid(),SIGUSR1);
+			//kill(child1,SIGUSR1);
+			printf("Send signal to child1:%d...\n",child1);
+			kill(getppid(),SIGUSR1);
 		}
 	}else{ //child1 process
+		//pause();
+		printf("Signal recieved from child2...\n");
 		printf("child1\n");
 		printf("pid: %d\nppid: %d\n\n",getpid(),getppid());
-		//kill(getppid(),SIGUSR1);
+		kill(getppid(),SIGUSR1);
 	}
 	return 0;
 }
